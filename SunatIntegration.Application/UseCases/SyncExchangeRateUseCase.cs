5ff4f429c7fb8b791675ee7a238434d4b72c67e7
Juan Gutierrez <juan@innova-funding.com>
2026-03-08 19:40:53 -0500

    [llamando a clase use case desde el function]

---
+++ SunatIntegration.Application/UseCases/SyncExchangeRateUseCase.cs
@@ -0,0 +1,33 @@
+﻿using SunatIntegration.Application.Interfaces;
+using SunatIntegration.Domain.Entities;
+using SunatIntegration.Domain.Interfaces;
+
+namespace SunatIntegration.Application.UseCases;
+
+public class SyncExchangeRateUseCase
+{
+    private readonly ISunatApiClient _sunatClient;
+    private readonly IExchangeRateRepository _repository;
+
+    public SyncExchangeRateUseCase(
+        ISunatApiClient sunatClient,
+        IExchangeRateRepository repository)
+    {
+        _sunatClient = sunatClient;
+        _repository = repository;
+    }
+
+    public async Task ExecuteAsync()
+    {
+        var rate = await _sunatClient.GetExchangeRateAsync(DateTime.Now);
+
+        var exchangeRate = new ExchangeRate
+        {
+           // DatePublic = rate.fecPublica,
+           //PriceSales = rate.valTipo,
+           // Pricepurchase = rate.valTipo
+        };
+
+        await _repository.SaveAsync(exchangeRate);
+    }
+}
--- SunatIntegration.Functions/Functions/SunatConsumer.cs
+++ SunatIntegration.Functions/Functions/SunatConsumer.cs
@@ -2,22 +2,26 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using SunatIntegration.Application.UseCases;
 
 namespace SunatIntegration.Functions.Functions;
 
 public class SunatConsumer
 {
     private readonly ILogger<SunatConsumer> _logger;
+    private readonly SyncExchangeRateUseCase _useCase;
 
-    public SunatConsumer(ILogger<SunatConsumer> logger)
+    public SunatConsumer(ILogger<SunatConsumer> logger, SyncExchangeRateUseCase useCase)
     {
         _logger = logger;
+        _useCase = useCase;
     }
 
     [Function("SunatConsumer")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
+        await _useCase.ExecuteAsync();
         return new OkObjectResult("Welcome to Azure Functions!");
     }
 }
