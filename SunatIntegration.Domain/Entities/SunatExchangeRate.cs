44474345f9b91edd9243b1cfa6a5d81c8f0a63d8
Juan Gutierrez <juan@innova-funding.com>
2026-03-12 14:46:16 -0500

    [formater]

--- SunatIntegration.Domain/Entities/SunatExchangeRate.cs
+++ SunatIntegration.Domain/Entities/SunatExchangeRate.cs
@@ -7,8 +7,8 @@ namespace SunatIntegration.Domain.Entities
         public DateTime DatePublic { get; set; }
         public decimal? PriceSales { get; set; }
         public decimal? Pricepurchase { get; set; }
-        public DateTime CreatedDate { get; set; }= DateTime.Now;
-        public DateTime ModifiedDate { get; set; }= DateTime.Now;
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime ModifiedDate { get; set; } = DateTime.Now;
 
     }
 }
